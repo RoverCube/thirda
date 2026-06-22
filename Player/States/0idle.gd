d52057b11190e0e1f4d0852601bf433345e4fb8d
RoverCube <rover.cube@proton.me>
2026-06-22 17:06:08 -0300

    added inputs on player

--- Player/States/0idle.gd
+++ Player/States/0idle.gd
@@ -1,1 +1,6 @@
 extends PlayerState
+
+func _check_change() -> void:
+	if not is_zero_approx(player.input_axies):
+		change_state.emit("walk")
+	
--- Player/player_class.gd
+++ Player/player_class.gd
@@ -4,7 +4,17 @@ extends CharacterBody3D
 @onready var buffer_timer: Timer = $JumpBufferTimer
 @onready var main  : Main = $"../.."
 @export var device : int = 1
-var input_axies    : float = 0
+
+var input_axies        : float = 0.0
+var input_crouch       : bool  = false
+var input_jump         : bool  = false
+var input_block        : bool  = false
+var input_light_punch  : bool  = false
+var input_strong_punch : bool  = false
+var input_light_kick   : bool  = false
+var input_strong_kick  : bool  = false
+var input_grab         : bool  = false
+var input_ultimate     : float = 0.0
 #var max_speed      : float = 5
 #var aceleration    : float = 8
 #var deceleration   : float = 12
@@ -25,15 +35,15 @@ var super_active         : bool = false
 func _ready() -> void:
 	max_health = character.max_health
 	health = max_health
-	
 	character.hurt_box.got_damaged.connect(_on_get_hurt)
+
+func _input(_event: InputEvent) -> void:
+	input_axies = Input.get_axis(input_name("Left"),input_name("Right"))
+	input_crouch = Input.is_action_just_pressed(input_name("Crouch"))
+	
 
 func input_name(a: String) -> String:
 	return str(device,a)
-
-func _input(event: InputEvent) -> void:
-	input_axies = Input.get_axis(input_name("Left"),input_name("Right"))
-
 func _on_get_hurt(hit: HitBox):
 	if hit.auto_freeze:
 		main.freeze(0,0.005 * hit.damage)
@@ -42,5 +52,4 @@ func _on_get_hurt(hit: HitBox):
 	super_active = false
 	
 	health_bar.value = health
-
 func _on_super_timer_timeout() -> void: super_active = false
